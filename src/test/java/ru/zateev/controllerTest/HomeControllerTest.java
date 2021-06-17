ba02c1919a47497c364d3171f67ee4bedab48f45
Aleksey Zateev <alekseyzateev@gmail.com>
2021-06-17 12:11:29 +0500

    testMock

--- src/test/java/ru/zateev/controllerTest/HomeControllerTest.java
+++ src/test/java/ru/zateev/controllerTest/HomeControllerTest.java
@@ -1,17 +1,73 @@
 package ru.zateev.controllerTest;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.zateev.Entity.Person;
+import ru.zateev.User;
+import ru.zateev.UserImpl;
 import ru.zateev.configuration.Conf;
+import ru.zateev.dao.PersonDao;
+import ru.zateev.dao.PersonDaoImpl;
 import ru.zateev.service.PersonService;
+import ru.zateev.service.PersonServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = Conf.class)
 public class HomeControllerTest {
+    @Rule
+    MockitoRule rule = MockitoJUnit.rule();
 
+    @InjectMocks
     @Autowired
-    private PersonService personService;
+    PersonService personService;
 
+    @Mock
+    @Autowired
+    PersonDao personDao;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void getAllPersonTest() {
+
+        User user = new UserImpl();
+        Person person = new Person();
+        Person person1 = new Person();
+        person.setName("Aleksey");
+        person.setSurname("Ivanov");
+        person.setMail("asda@mail.com");
+        person.setAge(22);
+        person1.setName("Ivan");
+        person1.setSurname("Andreev");
+        person1.setMail("asdas@mail.com");
+        person1.setAge(33);
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(person);
+        personList.add(person1);
+        when(personDao.getAllPersons(user)).thenReturn(personList);
+        List<Person> persons = personService.getAllPersons(user);
+        Assertions.assertEquals(2, persons.size());
+
+    }
 }
